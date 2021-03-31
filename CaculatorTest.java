adda03a83dd7068ce08d531831bb4e74cf6ef118
jangseungjun <35719368+jangseungjun@users.noreply.github.com>
2021-03-31 21:55:23 +0900

    Update CaculatorTest.java

--- CaculatorTest.java
+++ CaculatorTest.java
@@ -22,14 +22,10 @@ public class Greeting
  
     public static void main(String[] args)
     {
-     System.out.println("Hello World");
+     
      System.out.println("Hello Kim!");
      System.out.println("Hello Lee!");
-     for (int i =0; i<10; i++) {
-      
-      System.out.println("Hello " + i);
-      
-         }
+     
     }    
 }
 public class CaculatorTest {
